4dd5ccf7a682a3cb35380b524af0d7b104de2858
Sanjida <sanji_b@outlook.com>
2024-03-22 16:29:02 +0000

    added heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,18 @@
-import './App.scss'
+import './App.scss';
+// import team from './team';
+
+
+const App = () => {
+
+
+  return (
+    <div>
+      <h1 className='ticket_heading'>Ticket Tracker</h1>
+    </div>
+
+  
+  )
+}
+
+
+export default App;
