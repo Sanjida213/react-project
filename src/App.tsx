296f1ca6f2e88a8d9d213867f2bd62e2e3e3d611
Sanjida <sanji_b@outlook.com>
2024-03-25 23:03:52 +0000

    added search by name

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,21 @@
 import './App.scss';
 import Employees from './components/Employee/Employee';
 import team from './team';
-import Counter from './components/Counter/Counter';
 
 // we want to put the counter component inside the employee component and then
 // instead of team[0] we want to map over it so it gets each team employee. we can do this in apps.tsx
 const App = () => {
-  
-// const employees = team.map(employee => (
-//   <div key={employee.id} className='employee'>
-//     <p>Name: {employee.name}</p>
-//     <p>Role: {employee.role}</p>
-//   </div>
-// ));
 
   return (
     <div className='ticket-container'>
+      
       <h1 className='ticket__heading'>Ticket Tracker</h1>
     
-    <div className='employees-tickets'>
+    <div className='ticket__employees'>
       
-      <section >
+      <section>
        <div className='ticket__employees-info'>
        <Employees employees={team}/>
-
        </div>
       </section>
 
--- src/components/Employee/Employee.tsx
+++ src/components/Employee/Employee.tsx
@@ -1,3 +1,4 @@
+import React, { FormEvent, useState } from "react";
 import "./Employee.scss";
 import Employee from "../../teamsTypes";
 import Counter from "../Counter/Counter";
@@ -9,33 +10,43 @@ type TeamProps = {
 
 
 const Employees = ({ employees }: TeamProps) => {
-  
-  // const employees = [...employee] /// maybe add employee here and then inside return map 
+  const [searchValue, setSearchValue] = useState<string>("");
 
+  const handleInput = (event: FormEvent<HTMLInputElement>) => {
+    const input = event.currentTarget.value.toLowerCase();
+    setSearchValue(input)
+  };
+
+  const filteredName = employees.filter((employee) => employee.name.toLowerCase().includes(searchValue))
+  
   return (
     <div className="employee-container">
-       <div className="employee-card">
-       {employees.map((employee) => (
-        <div className="employee-info" key={employee.id}>
-          <div>
+
+      <input
+        type="text"
+        placeholder="Search by name..."
+        value={searchValue}
+        onChange={handleInput}
+      />
+
+       {filteredName.map((employee) => (
+      <div className="employee-info" key={employee.id}>
+          
+        <div className="employee__name-role">
           <p className="employee-content__names">
            Name: {employee.name}
          </p>
-       
          <p className="employee-content__roles">
            Role: {employee.role}
          </p>
         </div>
 
-          <Counter heading="Counter"/>
+        <Counter heading="Counter"/>
          
-        </div>
+      </div>
         ))};
-       </div>
        
     </div>
   );
 };
 export default Employees;
-
-
